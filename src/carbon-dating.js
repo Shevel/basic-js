b0f083ffd1e49a8b019af875f4aa642beb5760a4
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-02-16 23:51:19 +0300

    feat: add carbon and cats task solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity){
+  if(typeof(sampleActivity) === "boolean" || sampleActivity === undefined 
+  || typeof(sampleActivity) === "number" || sampleActivity === null 
+  || typeof(sampleActivity) === "object" || !sampleActivity){
+    return false;
+  }
+  if (typeof sampleActivity != 'string' || /[a-zA-Z]/g.test(sampleActivity) || (sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY)) {
+    return false;
+  }
+  return Math.ceil(( Math.log(15 / parseFloat(sampleActivity) )) / (0.693 / 5730));
 };
