a9b2367960096bffb6c4166fad2f0b2f6ce6f899
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-02-18 19:46:01 +0300

    feat: add solution of transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,25 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error('err');
+    let resultArray = [];
+    if (arr.length < 1) {return [];}
+    for (let i = 0, length = arr.length; i < length; i++) {
+        if(arr[i] === '--discard-next') {
+            i++;
+            continue;
+        }
+        if(arr[i] === '--discard-prev') {
+            resultArray.length !== 0 ? resultArray.pop() : null;
+            continue;
+        }
+        if(arr[i] === '--double-next') {
+            i < (length - 1) ? resultArray.push(arr[i + 1]) : null;
+            continue;
+        }
+        if(arr[i] === '--double-prev') {
+            i >= 1 ? resultArray.push(arr[i - 1]) : null;
+            continue;
+        }
+        resultArray.push(arr[i]);
+    }
+    return resultArray;
 };
