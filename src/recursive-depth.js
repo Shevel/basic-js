6182cd4dabb73e5f398d04c3d4aaf445d5e08a33
Vladislav Shevel <vladislav.shevel@gmail.com>
2020-03-01 23:41:07 +0300

    fix:add recursive solution of task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,8 @@
 module.exports = class DepthCalculator {
+
     calculateDepth (arr) {
-        let count = 0;
-        if(arr.length > 0){count = 1;} 
-        let i = 0;
-        let isArray = true;
-        while(isArray){
-            for(i = 0;i < arr.length;i++){
-                Array.isArray(arr[i]) ? isArray = true : isArray = false;
-                if(isArray === true){
-                    count++;
-                    //arr = arr.flat();
-                    arr = arr.reduce((acc, val) => acc.concat(val), []);
-                    i--;
-                }
-            }
-        }
-        return count;
+        if(Array.isArray(arr) && arr.length === 0){return 1}
+        if(!Array.isArray(arr)){ return 0}
+        return 1 + Math.max.apply(null, arr.map((a) => this.calculateDepth(a)));
     }
 };
